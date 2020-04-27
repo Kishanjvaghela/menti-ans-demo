06fcad7fe9ba3591fe3d536105246e2f097b840e
Kishan Vaghela <kishanjvaghela@gmail.com>
2020-04-27 10:47:51 +0800

    used rxjs

--- lib/index.ts
+++ lib/index.ts
@@ -1,56 +1,47 @@
 import fetch from "node-fetch";
+import { from } from "rxjs";
+import { map, switchMap, mergeMap, toArray, filter } from "rxjs/operators";
+
 import { QueResponse } from "./models/QueResponse";
 import { Question } from "./models/Question";
 import { ResultResponse } from "./models/ResultResponse";
 import { Result } from "./models/Result";
+import { Choice } from "./models/Choice";
 
-const getQuestions = async (id: string) => {
+const getQuestions = (id: string) => {
   if (!id) {
     console.log("Please add proper id");
   }
   console.log("Loading...", id);
-  try {
-    const response = await fetch(
-      `https://www.menti.com/core/vote-keys/${id}/series`
-    );
-    if (response.status === 200) {
-      const responseText = await response.text();
-      const queResponse: QueResponse = JSON.parse(responseText);
+  const questionPromise = fetch(
+    `https://www.menti.com/core/vote-keys/${id}/series`
+  );
+  const observable = from(questionPromise).pipe(
+    switchMap((response) => response.json()),
+    map((queResponse) => {
       console.log("Name", queResponse.name);
       console.log("Total Slides", queResponse.questions.length);
-      if (queResponse.questions.length > 0) {
-        const questions = queResponse.questions.filter(
-          (question: Question) => question.type === "quiz"
-        );
-        console.log("Total Questions", questions.length);
-        const updatedQuestions: Question[] = [];
-        await questions.forEach(
-          async (question: Question) => {
-            const resultResponse = await fetch(
-              `https://api.mentimeter.com/questions/${question.admin_key}/result`
-            );
-            if (resultResponse.status === 200) {
-              const resultResponseText = await resultResponse.text();
-              const result: ResultResponse = JSON.parse(resultResponseText);
-              question.choices = result.results.questions[question.id].choices;
-              console.log(result.results.questions[question.id].choices);
-            } else {
-              console.log("Error while getting result", resultResponse.status);
-            }
-            updatedQuestions.push(question);
-          }
-        );
-        updatedQuestions.forEach((question: Question) => {
-          console.log(question.question);
-          console.log(question.choices);
-        });
-      }
-    } else {
-      console.log("Error", response.status);
-    }
-  } catch (error) {
-    console.log("Error", error);
-  }
+      return queResponse;
+    }),
+    mergeMap((queResponse: QueResponse) => queResponse.questions),
+    filter((question: Question) => question.type === "quiz"),
+    mergeMap((question: Question, val: number) => {
+      return from(
+        fetch(`https://api.mentimeter.com/questions/${question.id}/result`)
+      ).pipe(
+        switchMap((response) => response.json()),
+        map((data: ResultResponse) => {
+          question.choices = data.results.questions[question.id].choices;
+          return question;
+        })
+      );
+    }),
+    toArray()
+  );
+
+  observable.subscribe((questions: Question[]) => {
+    console.log("Total Questions", questions.length);
+  });
 };
 
 // npm run menti 83c3zmzy28
